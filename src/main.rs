a457d057b32b99592066af7aff5e917d73d4a188
Kevin Basta <kevinbastadeveloper@gmail.com>
2025-12-18 13:05:11 -0500

    request response flow

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,64 @@
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
+use std::{env, io::{Read, Write}, net::{TcpListener, TcpStream}, thread};
 
 
 
 fn protocol(stream: &mut TcpStream) {
-    println!("HIHIHI Stream :)!");
-
     let mut buffer: [u8; 128] = [0; 128]; 
-    let result = stream.read(&mut buffer).unwrap();
+    let mut result = stream.read(&mut buffer).unwrap();
+    println!("read request: ");
     
     for byte in buffer.iter() {
-        print!("{byte}");
+        let char = *byte as char;
+        print!("{char}");
     }
-}
-
-fn acceptPeer() {
-    let listener = TcpListener::bind("0.0.0.0:9090").unwrap();
-    let addr = listener.local_addr().unwrap();
-    println!("{addr}");
-
-    // spawn a detatched thread for each incoming connection
-    for stream in listener.incoming() {
-        println!("accepted");
-        thread::spawn(move || protocol(&mut stream.unwrap()));
-    }
+    println!("");
+    
+    result = stream.write(b"peer server response").unwrap();
+    println!("sent response");
 }
 
 fn requestPeer() {
-    let mut stream = TcpStream::connect("127.0.0.1:7070").unwrap();
+    let mut stream = TcpStream::connect("localhost:9090").unwrap();
+    println!("connected");
+    
+    let mut result = stream.write(b"request").unwrap();
+    println!("sent request");
+    
+    let mut buffer: [u8; 128] = [0; 128]; 
+    result = stream.read(&mut buffer).unwrap();
+    println!("got response: ");
 
-    let result = stream.write(b"").unwrap();
-    println!("{result}");
+    for byte in buffer.iter() {
+        let char = *byte as char;
+        print!("{char}");
+    }
+    println!("");
+
+}
+
+fn acceptPeer() {
+    let listener = TcpListener::bind("localhost:9090").unwrap();
+    let addr = listener.local_addr().unwrap();
+    println!("{addr}");
+    
+    // spawn a detatched thread for each incoming connection
+    while (true) {
+        let (mut stream, addr) = listener.accept().unwrap();
+        println!("Spawning thread for {addr}");
+        thread::spawn(move || protocol(&mut stream));
+
+    }
 }
 
 fn main() {
     println!("Hello world");
 
-    acceptPeer();
-    //requestPeer();
+    let args: Vec<String> = env::args().collect();
+    let doRequest = &args[1];
+    
+    if (doRequest == "1") {
+        requestPeer();
+    } else {
+        acceptPeer();
+    }
 }
